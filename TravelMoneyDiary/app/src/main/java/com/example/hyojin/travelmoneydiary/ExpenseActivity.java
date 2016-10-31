8ffc4aefca1bd58276cee1723f3ce135ced235ec
Daeho Choi <khsrhhh@gmail.com>
2016-10-31 11:57:37 +0900

    database 작업 중...데이터 다 날라가서 다시 commit함 ㅡㅡ

--- TravelMoneyDiary/app/src/main/java/com/example/hyojin/travelmoneydiary/ExpenseActivity.java
+++ TravelMoneyDiary/app/src/main/java/com/example/hyojin/travelmoneydiary/ExpenseActivity.java
@@ -1,5 +1,6 @@
 package com.example.hyojin.travelmoneydiary;
 
+/*
 import android.content.Intent;
 import android.database.Cursor;
 import android.support.v7.app.AppCompatActivity;
@@ -51,4 +52,105 @@ public class ExpenseActivity extends AppCompatActivity {
         finish();
     }
 }
+
+*/
+
+
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    SQLDB expense_db;       //myDB-> SQLDB  my->expense_db
+    EditText edittext1, edittext2, edittext3;
+    TextView cost, date, content;   //금액, 날짜, 내용
+    Button btn1, btn2, btn3;
+    SQLiteDatabase sql;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_expense);      //activity_expense? activity_main?
+
+        edittext1 = (EditText) findViewById(R.id.editText1);
+        edittext2 = (EditText) findViewById(R.id.editText2);
+        edittext3 = (EditText) findViewById(R.id.editText3);
+
+        cost = (TextView) findViewById(R.id.cost);
+        date = (TextView) findViewById(R.id.date);
+        content = (TextView) findViewById(R.id.content);
+
+        btn1 = (Button) findViewById(R.id.button1);
+        btn2 = (Button) findViewById(R.id.button2);
+        btn3 = (Button) findViewById(R.id.button3);
+
+        expense_db = new SQLDB(this);
+
+        btn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                sql = expense_db.getWritableDatabase();
+                expense_db.onUpgrade(sql, 1, 2);
+                sql.close();
+            }
+        });
+
+        btn2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                sql = expense_db.getReadableDatabase();
+                Cursor cursor;
+                cursor = sql.rawQuery("SELECT * FROM MEMBER;", null);
+                String cost2 = "금액" + "\r\n";
+                String date2 = "날짜" + "\r\n";
+                String content2 = "내용" + "\r\n";
+
+                while (cursor.moveToNext()) {
+                    cost2 += cursor.getString(0) + "\r\n";
+                    date2 += cursor.getString(1) + "\r\n";
+                    content2 += cursor.getString(2) + "\r\n";
+                }
+                cost.setText(cost2);
+                date.setText(date2);
+                content.setText(content2);
+                cursor.close();
+                sql.close();
+            }
+        });
+
+        btn3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                sql = expense_db.getWritableDatabase();
+                sql.execSQL("INSERT INTO member VALUES(" + edittext1.getText().toString() + ",'"
+                        + edittext2.getText().toString() + "','"
+                        + edittext3.getText().toString() + "');");
+                sql.close();
+                Toast.makeText(getApplicationContext(), "정보가 저장되었습니다.", Toast.LENGTH_LONG).show();
+            }
+        });
+
+
+    }
+}
+
+
+
+
+
+
+
 
