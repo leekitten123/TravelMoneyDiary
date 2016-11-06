4f99ee13e8f3ddd08e07291c4ee1b603ab45fefb
Youngseok Ji <jiys5164@naver.com>
2016-11-06 23:52:11 +0900

    show income data in income.db file

--- TravelMoneyDiary/app/src/main/java/com/example/hyojin/travelmoneydiary/SearchActivity.java
+++ TravelMoneyDiary/app/src/main/java/com/example/hyojin/travelmoneydiary/SearchActivity.java
@@ -12,13 +12,15 @@ import java.util.ArrayList;
 
 public class SearchActivity  extends AppCompatActivity {
 
-    DBManager dbManager = new DBManager(this, "expense.db", null, 1);
-    DBAdapter adapter;
+    DBManager dbManager_expense = new DBManager(this, "expense.db", null, 1);
+    DBManager dbManager_income = new DBManager(this, "income.db", null, 1);
+    DBAdapter adapter_expense, adapter_income;
 
-    ArrayList<UsageList> ul = new ArrayList<>();
+    ArrayList<UsageList> ul_expense = new ArrayList<>();
+    ArrayList<UsageList> ul_income = new ArrayList<>();
 
     Button ButtonSearch;
-    ListView ExpenseListView;
+    ListView ExpenseListView, IncomeListView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,13 +30,18 @@ public class SearchActivity  extends AppCompatActivity {
         ButtonSearch = (Button) findViewById(R.id.button_Search);
 
         ExpenseListView = (ListView) findViewById(R.id.expenseListView);
+        IncomeListView = (ListView) findViewById(R.id.incomeListView);
 
         ButtonSearch.setOnClickListener(new View.OnClickListener() {
             public void onClick (View v) {
-                ul.clear();
-                dbManager.getResult(ul);
-                adapter = new DBAdapter(SearchActivity.this, ul, R.layout.expense_row);
-                ExpenseListView.setAdapter(adapter);
+                dbManager_expense.getResult(ul_expense);
+                adapter_expense = new DBAdapter(SearchActivity.this, ul_expense, R.layout.expense_row);
+                ExpenseListView.setAdapter(adapter_expense);
+
+                dbManager_income.getResult(ul_income);
+                adapter_income = new DBAdapter(SearchActivity.this, ul_income, R.layout.income_row);
+                IncomeListView.setAdapter(adapter_income);
+
                 Toast.makeText(SearchActivity.this, "조회 완료", Toast.LENGTH_SHORT).show();
             }
         });
