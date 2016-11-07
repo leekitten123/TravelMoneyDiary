491e0d8a858d93beca015bdf08ec1b8434eb6e7c
LeeSangWook <leekitten123@hanmail.net>
2016-11-07 18:56:44 +0900

    Success Making Expense_graph by LSW 2016.11.07
    Reviewed at http://reviews.cs.kookmin.ac.kr/r/518/

--- TravelMoneyDiary/app/src/main/java/com/example/hyojin/travelmoneydiary/SearchActivity.java
+++ TravelMoneyDiary/app/src/main/java/com/example/hyojin/travelmoneydiary/SearchActivity.java
@@ -4,6 +4,7 @@ package com.example.hyojin.travelmoneydiary;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ListView;
@@ -29,8 +30,8 @@ public class SearchActivity  extends AppCompatActivity {
     ListView ExpenseListView;
 
     private PieChart expense_Chart ;
-    private float[] yData = { 5, 10, 15, 30, 40 };
-    private String[] xData = { "Sony", "Huawei", "LG", "Apple", "Samsung" };
+    float[] yData = { 5, 10, 15, 30, 40 };
+    String[] xData = { "Sony", "Huawei", "LG", "Apple", "Samsung" };
 
 
 
@@ -45,13 +46,7 @@ public class SearchActivity  extends AppCompatActivity {
 
         expense_Chart = (PieChart) findViewById(R.id.expense_Chart);
         // configure pie chart
-        expense_Chart.setUsePercentValues(true); // ???
-        expense_Chart.setDescription("Expense Chart"); // 우측 하단의 제목
-        // enable rotation of the chart by touch
-        expense_Chart.setRotationAngle(0);
-        expense_Chart.setRotationEnabled(true); // 터치해서 돌리기 가능
-        // add data
-        addData();
+
 
         ButtonSearch.setOnClickListener(new View.OnClickListener() {
             public void onClick (View v) {
@@ -59,58 +54,67 @@ public class SearchActivity  extends AppCompatActivity {
                 dbManager.getResult(ul);
                 adapter = new DBAdapter(SearchActivity.this, ul, R.layout.expense_row);
                 ExpenseListView.setAdapter(adapter);
+
+                for (int i = 0 ; i <ul.size() ; i++) {
+                    xData[i] = ul.get(i).content;
+                    yData[i] = ul.get(i).price;
+                }
+                expense_Chart.setUsePercentValues(true); // ???
+                expense_Chart.setDescription("Expense Chart"); // 우측 하단의 제목
+                // enable rotation of the chart by touch
+                expense_Chart.setRotationAngle(0);
+                expense_Chart.setRotationEnabled(true); // 터치해서 돌리기 가능
+
+                // add data
+                addData();
+
                 Toast.makeText(SearchActivity.this, "조회 완료", Toast.LENGTH_SHORT).show();
             }
         });
     }
 
     private void addData() {
+
         ArrayList<Entry> yVals1 = new ArrayList<Entry>();
-
-
-        //yvlas에 ydata 추가
+        //yVals에 ydata 추가
         for (int i = 0; i < yData.length; i++)
             yVals1.add(new Entry(yData[i], i));
 
-        
-        ArrayList<String> xVals = new ArrayList<String>();
 
+        ArrayList<String> xVals = new ArrayList<String>();
+        //xVals에 xdata 추가
         for (int i = 0; i < xData.length; i++)
             xVals.add(xData[i]);
 
         // create pie data set
-        PieDataSet dataSet = new PieDataSet(yVals1, "Market Share");
-        dataSet.setSliceSpace(3);
-        dataSet.setSelectionShift(5);
+        PieDataSet dataSet = new PieDataSet(yVals1, "kk"); // 우측 최하단에 스트링 출력
+        dataSet.setSliceSpace(3); // 숫자가 크면 원 그래프에서 데이터간의 간격 증가
+        dataSet.setSelectionShift(5); // 숫자가 크면 반지름 감소
 
         // add many colors
         ArrayList<Integer> colors = new ArrayList<Integer>();
-
         for (int c : ColorTemplate.VORDIPLOM_COLORS)
             colors.add(c);
-
         for (int c : ColorTemplate.JOYFUL_COLORS)
             colors.add(c);
-
         for (int c : ColorTemplate.COLORFUL_COLORS)
             colors.add(c);
-
         for (int c : ColorTemplate.LIBERTY_COLORS)
             colors.add(c);
-
         for (int c : ColorTemplate.PASTEL_COLORS)
             colors.add(c);
-
         colors.add(ColorTemplate.getHoloBlue());
         dataSet.setColors(colors);
 
         // instantiate pie data object now
-        PieData data = new PieData(xVals, dataSet);
-        data.setValueFormatter(new PercentFormatter());
-        data.setValueTextSize(11f);
-        data.setValueTextColor(Color.GRAY);
+        PieData data = new PieData(xVals, dataSet); // x를 Pie dataset에 넣어준다.
 
-        expense_Chart.setData(data);
+
+        data.setValueFormatter(new PercentFormatter());
+        data.setValueTextSize(11f); // 원그래프에서 값 텍스트 크기
+        data.setValueTextColor(Color.GRAY); // 그래프 안의 값 텍스트의 색
+
+        expense_Chart.setData(data); // Pie data값 추가
 
         // undo all highlights
         expense_Chart.highlightValues(null);
