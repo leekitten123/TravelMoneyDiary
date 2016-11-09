920a0221c1298aa7816769bcc714a72105008c9d
LeeSangWook <leekitten123@hanmail.net>
2016-11-09 19:20:31 +0900

    Success Expense_Page Exchange Rate

--- TravelMoneyDiary/app/src/main/java/com/example/hyojin/travelmoneydiary/ExpenseActivity.java
+++ TravelMoneyDiary/app/src/main/java/com/example/hyojin/travelmoneydiary/ExpenseActivity.java
@@ -28,7 +28,8 @@ public class ExpenseActivity extends AppCompatActivity {
     EditText EditTextDate, EditTextContent, EditTextPrice;
     TextView TextViewPrice;
 
-    static int numCountry = 4 ;
+    static int numCountry = 4;
+    MyExchangeRate mER = new MyExchangeRate();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -41,10 +42,11 @@ public class ExpenseActivity extends AppCompatActivity {
         iDate = today.get(Calendar.DAY_OF_MONTH);
         TextView caltv = (TextView) findViewById(R.id.expensedate);
         caltv.setText(iYear + "년 " + iMonth + "월 " + iDate + "일");
-        iMonth-=1;
-        Button contrybtn = (Button)findViewById(R.id.expenseCountry);
+        iMonth -= 1;
+        Button contrybtn = (Button) findViewById(R.id.expenseCountry);
         registerForContextMenu(contrybtn);
 
+        mER.setExcahngeRate();
 
         ButtonExpense = (Button) findViewById(R.id.button_Expense);
         ButtonIncome = (Button) findViewById(R.id.button_Income);
@@ -54,25 +56,27 @@ public class ExpenseActivity extends AppCompatActivity {
 
         String temp = etexpensedate.getText().toString();
 
-        final int dateint = iYear*10000+(iMonth+1)*100+iDate;
+        final int dateint = iYear * 10000 + (iMonth + 1) * 100 + iDate;
 
         EditTextContent = (EditText) findViewById(R.id.editText_Content);
         EditTextPrice = (EditText) findViewById(R.id.editText_Price);
         TextViewPrice = (TextView) findViewById(R.id.textView_Price);
 
         EditTextPrice.addTextChangedListener(new TextWatcher() {
-            public void onTextChanged(CharSequence s, int start, int before, int count) {}
-            public void beforeTextChanged(CharSequence s, int start, int count, int after) {}
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+            }
+
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+            }
+
             public void afterTextChanged(Editable arg0) {
                 // 입력이 끝났을 때
-                MyExchangeRate mER = new MyExchangeRate() ;
-
                 mER.seeAfterMoney(numCountry, EditTextPrice, TextViewPrice);
             }
         });
 
         ButtonSave.setOnClickListener(new View.OnClickListener() {
-            public void onClick (View v) {
+            public void onClick(View v) {
                 if (EditTextContent.getText().toString().equals("") || EditTextPrice.getText().toString().equals("")) {
                     Toast.makeText(ExpenseActivity.this, "잘못된 입력이 있습니다.", Toast.LENGTH_SHORT).show();
                     clear();
@@ -86,39 +90,39 @@ public class ExpenseActivity extends AppCompatActivity {
         });
     }
 
-    public void onClick_Income (View v) {
-        Intent intent_Income = new Intent (getApplicationContext(), IncomeActivity.class);
-        startActivity (intent_Income);
+    public void onClick_Income(View v) {
+        Intent intent_Income = new Intent(getApplicationContext(), IncomeActivity.class);
+        startActivity(intent_Income);
         finish();
     }
 
     void clear() {
-    //    EditTextDate.setText("");
+        //    EditTextDate.setText("");
         EditTextContent.setText("");
         EditTextPrice.setText("");
     }
 
-    public void clickexpensedate(View v){
-            DatePickerDialog.OnDateSetListener dateSetListener = new DatePickerDialog.OnDateSetListener() { //datepicker
+    public void clickexpensedate(View v) {
+        DatePickerDialog.OnDateSetListener dateSetListener = new DatePickerDialog.OnDateSetListener() { //datepicker
 
 
-                @Override
-                public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
-                    // TODO Auto-generated method stub
-                    monthOfYear+=1;
-                    TextView caltv = (TextView) findViewById(R.id.expensedate);           // calendartv객체를 받아옴
-                    caltv.setText(year + "년 " + monthOfYear + "월 " + dayOfMonth + "일");           //선택한 년원일으로 caltv에 날짜를 적음
+            @Override
+            public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
+                // TODO Auto-generated method stub
+                monthOfYear += 1;
+                TextView caltv = (TextView) findViewById(R.id.expensedate);           // calendartv객체를 받아옴
+                caltv.setText(year + "년 " + monthOfYear + "월 " + dayOfMonth + "일");           //선택한 년원일으로 caltv에 날짜를 적음
 
-                    iYear = year;                 //이부분을 하지 않으면 클릭하여서 날짜를 바꾸면 그게 DatePickerDialog에 반영되지 않음
-                    iMonth = monthOfYear-1;
-                    iDate = dayOfMonth;
-                }
-            };
+                iYear = year;                 //이부분을 하지 않으면 클릭하여서 날짜를 바꾸면 그게 DatePickerDialog에 반영되지 않음
+                iMonth = monthOfYear - 1;
+                iDate = dayOfMonth;
+            }
+        };
 
-            new DatePickerDialog(this, dateSetListener, iYear, iMonth, iDate).show();      //dateoicker를 보여줌
+        new DatePickerDialog(this, dateSetListener, iYear, iMonth, iDate).show();      //dateoicker를 보여줌
     }
 
-    public void onCreateContextMenu (ContextMenu menu, View v, ContextMenu.ContextMenuInfo menuInfo){
+    public void onCreateContextMenu(ContextMenu menu, View v, ContextMenu.ContextMenuInfo menuInfo) {
         super.onCreateContextMenu(menu, v, menuInfo);
         // 컨텍스트 메뉴가 최초로 한번만 호출되는 콜백 메서드
 
@@ -130,38 +134,43 @@ public class ExpenseActivity extends AppCompatActivity {
         menu.add(0, 5, 100, "한화");
     }
 
-    public boolean onContextItemSelected (MenuItem item){
-        Button countrybtn = (Button)findViewById(R.id.expenseCountry);
+    public boolean onContextItemSelected(MenuItem item) {
+        Button countrybtn = (Button) findViewById(R.id.expenseCountry);
         // 롱클릭했을 때 나오는 context Menu 의 항목을 선택(클릭) 했을 때 호출
 
         switch (item.getItemId()) {
             case 1:// 달러
                 countrybtn.setBackgroundResource(R.drawable.dollor);
-                numCountry = 0 ;
+                numCountry = 0;
                 return true;
 
             case 2:// 엔화
                 countrybtn.setBackgroundResource(R.drawable.jpy);
-                numCountry = 1 ;
+                numCountry = 1;
                 return true;
 
             case 3:// 유로
                 countrybtn.setBackgroundResource(R.drawable.eur);
-                numCountry = 2 ;
+                numCountry = 2;
                 return true;
 
             case 4:// 위안
                 countrybtn.setBackgroundResource(R.drawable.cny);
-                numCountry = 4 ;
+                numCountry = 3;
                 return true;
 
             case 5: // 한국돈
                 countrybtn.setBackgroundResource(R.drawable.krw);
-                numCountry = 4 ;
+                numCountry = 4;
                 return true;
         }
 
         return super.onContextItemSelected(item);
+    }
+
+    public void onClick_Country(View v) {
+        mER.setBeforeMoney(EditTextPrice);
+        mER.seeAfterMoney(numCountry, EditTextPrice, TextViewPrice);
     }
 }
 
--- TravelMoneyDiary/app/src/main/java/com/example/hyojin/travelmoneydiary/MyExchangeRate.java
+++ TravelMoneyDiary/app/src/main/java/com/example/hyojin/travelmoneydiary/MyExchangeRate.java
@@ -17,36 +17,46 @@ public class MyExchangeRate {
     int beforeMoney ;
     int afterMoney ;
 
-    float[] exchangeRate = new float[12] ; // 각 나라의 환율
+
+    private static String Address;
+    private static URL url;
+    private static BufferedReader br;
+    private static HttpURLConnection conn;
+    private static String protocol = "GET";
+
+    static float[] exchangeRate = new float[12] ; // 각 나라의 환율
 
     void setExcahngeRate() {
         new Thread() {
             public void run() {
                 try {
-                    String Address = "http://fx.kebhana.com/fxportal/jsp/RS/DEPLOY_EXRATE/fxrate_all.html";        //자료를 받아올 주소
-                    URL url = new URL(Address);
-                    HttpURLConnection conn = (HttpURLConnection) url.openConnection();        //연결해줌;
-                    BufferedReader br = new BufferedReader(new InputStreamReader(conn.getInputStream(), "MS949")); //MS949는 한글표현;
-                    String protocol = "GET";
-                    conn.setRequestMethod(protocol);        //언어타입과 문자타입지정
-
-                    int i = 0 ;
-
-                    String line;
-                    String tempLine;
-
-                    while ((line = br.readLine()) != null) {//웹에서 소스보기를 통하여 원하는 부분의 데이터를 받아옴
-                        if (line.startsWith("<td class='buy'>")) { // 환율 받기 1
-                            tempLine = line.replace("<td class='buy'>", "").replace("</td>", "");
-                            exchangeRate[i] = Float.parseFloat(tempLine);
-                            i++;
-                        }
-                    }
+                    exchange() ;
                 } catch (Exception e) {
                     e.printStackTrace();
                 }
             }
         }.start();
+    }
+
+    void exchange() throws Exception{
+        Address = "http://fx.kebhana.com/fxportal/jsp/RS/DEPLOY_EXRATE/fxrate_all.html";        //자료를 받아올 주소
+        url = new URL(Address);
+        conn = (HttpURLConnection) url.openConnection();        //연결해줌
+        conn.setRequestMethod(protocol);        //언어타입과 문자타입지정
+        br = new BufferedReader(new InputStreamReader(conn.getInputStream(), "MS949")); //MS949는 한글표현
+
+        int i = 0 ;
+
+        String line ;
+        String tempLine ;
+
+        while ((line = br.readLine()) != null) {//웹에서 소스보기를 통하여 원하는 부분의 데이터를 받아옴
+            if (line.startsWith("<td class='buy'>")) { // 환율 받기 1
+                tempLine = line.replace("<td class='buy'>", "").replace("</td>", "");
+                exchangeRate[i] = Float.parseFloat(tempLine) ;
+                i++ ;
+            }
+        }
     }
 
     void setBeforeMoney(EditText editText) {
@@ -58,7 +68,6 @@ public class MyExchangeRate {
     }
 
     void seeAfterMoney(int numCountry, EditText editText, TextView textView){
-        setExcahngeRate();
         setBeforeMoney(editText);
 
         switch (numCountry) {
@@ -66,7 +75,7 @@ public class MyExchangeRate {
                 afterMoney = beforeMoney * (int)exchangeRate[4] ;
                 break;
             case 1:
-                afterMoney = beforeMoney * (int)exchangeRate[5] ;
+                afterMoney = beforeMoney * (int)exchangeRate[5] / 100 ;
                 break ;
             case 2:
                 afterMoney = beforeMoney * (int)exchangeRate[6] ;
